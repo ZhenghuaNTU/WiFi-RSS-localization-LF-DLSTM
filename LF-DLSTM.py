6847654c9cf25d508c03727f69dbc6605757c13c
ZhenghuaNTU <31753952+ZhenghuaNTU@users.noreply.github.com>
2018-09-27 17:18:49 +0800

    Update LF-DLSTM.py

--- LF-DLSTM.py
+++ LF-DLSTM.py
@@ -1,36 +1,44 @@
+### at the begin
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+#####
+
 import numpy as np
-import matplotlib.pyplot as plt
-#import pandas
 import math
-import keras.callbacks
-from keras.models import Sequential
-from keras.layers import Dense,Dropout
-from keras.layers import LSTM, Merge
-from keras.optimizers import SGD
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-from keras.objectives import mean_absolute_percentage_error as MAPE
-import scipy.io as scio
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convolution1D, Embedding, LSTM,Activation
+from keras.layers.wrappers import Bidirectional
+from keras.layers.merge import Concatenate
 from keras.layers.normalization import BatchNormalization
-# fix random seed for reproducibility
+import numpy as np
+import scipy.io as scio
+from sklearn import preprocessing
+from keras.utils import np_utils
+from sklearn.metrics import precision_score
+import keras.callbacks
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+
 np.random.seed(7)
 
-#alldata = scio.loadmat('dataset/trainAndtestData_norConf_czh3.mat')  
-alldata = scio.loadmat('dataset/trainAndtestData.mat')   #creatData10_30_DLSTM
+alldata = scio.loadmat('SimData1.mat')  
 trainData = alldata['trainData']
 testData = alldata['testData']
 trainTarget = alldata['trainTarget']
 testTarget = alldata['testTarget']
 
 # calculate mean of root mean squared error 
-def ME_get(truey,predy):
+def MAPE_get(truey,predy):
     temp = 0
     for i in range(len(truey[:,0])):
         temp = temp + math.sqrt(2*mean_squared_error(predy[i],truey[i]))
     mapescore = temp/len(truey[:,0])
     return mapescore
 
-class EpochME(keras.callbacks.Callback):
+class EpochMAPE(keras.callbacks.Callback):
     def __init__(self):
         self.mapes = []
         self.val_mapes = []
@@ -42,26 +50,31 @@ class EpochME(keras.callbacks.Callback):
         trainPredict = model.predict(trainData, batch_size=32)
         testPredict = model.predict(testData,batch_size=32)
             
-        acc = ME_get(trainTarget,trainPredict)
-        val_acc = ME_get(testTarget,testPredict)
+        acc = MAPE_get(trainTarget,trainPredict)
+        val_acc = MAPE_get(testTarget,testPredict)
                 #print('acc:'+str(acc)+' - val_acc:'+str(val_acc))
         self.mapes.append(acc)
         self.val_mapes.append(val_acc)
         self.predicted_y.append(testPredict)
 
+hidDim = [40]
+for i in range(len(hidDim)):
+    model_input = Input(shape=(trainData.shape[1],trainData.shape[2]))
+    lstm_1 = LSTM(30,activation='sigmoid',return_sequences=True)(model_input)
+    lstm_2 = LSTM(hidDim[i],activation='sigmoid',return_sequences=False)(lstm_1)
+    drop1 = Dropout(0.5)(lstm_2) 
 
-model = Sequential()
-model.add(LSTM(30, input_dim= 9, activation='sigmoid', return_sequences=True))  #look_back
-model.add(LSTM(40, activation='sigmoid', return_sequences=False))
-model.add(Dropout(0.5))
-model.add(Dense(60,activation='sigmoid'))
-model.add(Dropout(0.5))
-
-model.add(Dense(2))
-model.compile(loss='mean_squared_error', optimizer='adam') #adam
+    dense_1 = Dense(60,activation='sigmoid')(drop1)#activation='sigmoid')) # 100
+    drop2 = Dropout(0.5)(dense_1) 
+    model_outputs = Dense(2)(drop2)#activation='sigmoid'))
+    model = Model(model_input, model_outputs)
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001,beta_1=0.9, beta_2=0.999, epsilon=1e-8))#'adam') #adam
 
 
-epochmapes = EpochME()
-history = model.fit(trainData, trainTarget, validation_data=(testData, testTarget), nb_epoch=60, batch_size=5, verbose=2, callbacks=[epochmapes]) 
+    epochmapes = EpochMAPE()
 
+    history = model.fit(trainData, trainTarget, validation_data=(testData, testTarget), nb_epoch=20, batch_size=5, verbose=2, callbacks=[epochmapes]) #0.33 validation_data=(testX, testY),
+
+    #print 'rawQLDdata_W'
+    print(epochmapes.val_mapes[-1])
 
